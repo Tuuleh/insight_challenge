4582e909909b3e9a442c5a4761855aa8db946f10
tuuli <tuuli.pollanen@gmail.com>
2014-10-31 13:03:14 -0700

    Added two heuristic strategies that clean up the puzzle up a bit for brute force search.

--- sudoku.py
+++ sudoku.py
@@ -1,96 +1,115 @@
 import csv
-from sys import argv
+from copy import deepcopy as copy
 
 def open_puzzle(csv_file):
-    with open(csv_file, 'r') as puzzle:
-        grid = puzzle.read()
-        puzzle.close()
-        return grid
+    sudoku_reader = csv.reader(open(csv_file, 'r'))
+    grid = [int(item) for row in sudoku_reader for item in row if item not in ("", "\n") and int(item) in digits or item == '0']
+    return grid
 
-#a function for returning cross products - useful for making indices for the squares
+#a function for returning cross products - used for making indices for the squares and for adding indices to the list of units
 def cross(A, B):
-    return [a+b for a in A for b in B]
+    if not type(B) is list:
+        B = [B]
+    if not type(A) is list:
+        A = [A]
+    return [str(a)+str(b) for a in A for b in B]
 
 digits   = [1,2,3,4,5,6,7,8,9]
-rows     = 'ABCDEFGHI'
-cols     = digits
+rows     = ['A','B','C','D','E','F','G','H','I']
+columns  = digits
 
-#creates an array containing all of the squares from A1, B1.. to I9.
-squares  = cross(rows, cols)
+#creates an array containing key strings for all of the squares from A1, B1.. to I9.
+squares  = cross(rows, columns)
 
-#computes an array representing all of the units (rows, column and boxes) as arrays of the squares they contain
-unitlist = ([cross(rows, c) for c in cols] +
-            [cross(r, cols) for r in rows] +
-            [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')])
+#computes an array representing all of the units (rows, column and boxes)
+# as arrays of the key strings of the squares they contain
+unitlist = ([cross(rows, column) for column in columns] +
+            [cross(row, columns) for row in rows] +
+            [cross(box_row, box_column) for box_row in (['A','B','C'],['D','E','F'],['G','H','I']) for box_column in ([1,2,3],[4,5,6],[7,8,9])])
 
 #creates a dictionary that matches each square with its corresponding units in the unit list
-units = dict((s, [u for u in unitlist if s in u]) for s in squares)
+units = dict((square, [unit for unit in unitlist if square in unit]) for square in squares)
 #creates a dictionary that matches each square with its corresponding peers in the unit list
-peers = dict((s, set(sum(units[s],[]))-set([s])) for s in squares) 
+peers = dict((square, (set(sum(units[square],[]))-set([square]))) for square in squares)
 
 
 def create_grid(grid):
-    values = [item for item in grid if item in digits or item == '0']
-    assert (len(values) == 81), "Your puzzle contains empty values!"
-    #creates a dictionary with squares as keys, containing their corresponding values
-    print dict(zip(squares, values)) 
-    return dict(zip(squares, values)) 
+    values = [item for item in grid if item in digits or item == 0]
+    #returns a dictionary with squares as keys, and the possible digits for the squares as values
+    return dict(zip(squares, values))
 
 def parse_grid(grid):
-    # 
-    # To start, every square can be any digit; then assign values from the grid.
-    values = dict((square, digits) for square in squares)
-    for square, digit in create_grid(grid).items():
+    values = dict((square, copy(digits)) for square in squares)
+    game_grid = create_grid(grid)
+    for square, digit in game_grid.items():
         if digit in digits and not assign(values, square, digit):
-            return False ## (Fail if we can't assign d to square s.)
+            # returns false if we fail to assign a valid digit to the particular square
+            return False
+    # returns values remaining after heuristic processing
     print values
     return values
 
 
-def assign(values, s, d):
-    """Eliminate all the other values (except d) from values[s] and propagate.
-    Return values, except return False if a contradiction is detected."""
-    other_values = values[s].replace(d, '')
-    if all(eliminate(values, s, d2) for d2 in other_values):
-        return values
-    else:
+def assign(values, square, digit):
+    # Tries to eliminate all digits but one from the square's possible values,
+    # and if possible, remove the last remaining digit from the square's peers.
+    # Returns false if a contradiction is discovered (the same digit must be
+    # contained in another peer, which is not possible by game rules).
+    other_values = copy(values[square])
+    try:
+        other_values.remove(digit)
+    except:
+        print "Your puzzle seems to be broken!"
         return False
 
-def eliminate(values, s, d):
-    """Eliminate d from values[s]; propagate when values or places <= 2.
-    Return values, except return False if a contradiction is detected."""
-    if d not in values[s]:
-        return values ## Already eliminated
-    values[s] = values[s].replace(d,'')
-    ## (1) If a square s is reduced to one value d2, then eliminate d2 from the peers.
-    if len(values[s]) == 0:
-        return False ## Contradiction: removed last value
-    elif len(values[s]) == 1:
-        d2 = values[s]
-        if not all(eliminate(values, s2, d2) for s2 in peers[s]):
+    if all(eliminate(values, square, next_digit) for next_digit in other_values):
+        return values
+    else:
+        print "Contradiction discovered, failed to assign."
+        return False
+
+def eliminate(values, square, digit):
+    # eliminates the digit from the square's possible values, and goes on
+    # to make changes to the peers if there is less than 2 values remaining
+    # or if there are less than 2 possible places for the digit.
+    # Returns the heuristically processed values if everything works, or returns
+    # false if a contradiction is discovered.
+    if digit not in values[square]:
+        # the digit was already eliminated earlier
+        return values
+
+    values[square].remove(digit)
+
+    # If the square is reduced to one last value, then that value cannot be contained
+    # in its peers. Eliminate that value from the peers.
+    if len(values[square]) == 0:
+        print "Contradiction discovered - removed the last value!"
+        return False
+    elif len(values[square]) == 1:
+        last_digit = values[square][0]
+        if not all(eliminate(values, peer_square, last_digit) for peer_square in peers[square]):
             return False
-    ## (2) If a unit u is reduced to only one place for a value d, then put it there.
-    for u in units[s]:
-        dplaces = [s for s in u if d in values[s]]
-    if len(dplaces) == 0:
-        return False ## Contradiction: no place for this value
-    elif len(dplaces) == 1:
-        # d can only be in one place in unit; assign it there
-        if not assign(values, dplaces[0], d):
+    # If only one possible place for the digit remains inside a unit, it must belong to that square.
+    for unit in units[square]:
+        digit_places = [unit_square for unit_square in unit if digit in values[unit_square]]
+        if len(digit_places) == 0:
+            print "Contradiction - I found no place for this value!"
             return False
+        elif len(digit_places) == 1:
+            if not assign(values, digit_places[0], digit):
+                return False
     return values
 
 
 def run():
-    csv_file = raw_input("Please enter then name of the .txt/.csv file containing the sudoku puzzle.\n>>")
+    csv_file = raw_input("Please enter then name of the .txt/.csv file containing the sudoku puzzle.\n>> ")
     if csv_file.lower().endswith(('.csv', '.txt')):
         try: 
             puzzle = open_puzzle(csv_file)
-            print puzzle
             parse_grid(puzzle)
-        except IOError, e: 
-            print e[1]
+        except IOError, error:
+            print error[1]
     else:
-        print "You'll need the puzzle in a .txt or .csv file to run the sudoku solver!"
+        print "You need the puzzle in a .txt or .csv file!"
 
 run()
